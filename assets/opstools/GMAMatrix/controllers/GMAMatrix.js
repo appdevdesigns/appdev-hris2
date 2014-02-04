2412d197452491d0a2baa7de6eb6d951aa20fd08
Johnny Hausman <johnny@appdevdesigns.net>
2014-02-04 10:21:22 +0700

    + initial GMA Matrix framework

---
+++ assets/opstools/GMAMatrix/controllers/GMAMatrix.js
@@ -0,0 +1,55 @@
+
+steal(
+        // List your Controller's dependencies here:
+        'appdev',
+//        'opstools/GMAMatrix/models/Projects.js',
+//        'appdev/widgets/ad_delete_ios/ad_delete_ios.js',
+//        'opstools/GMAMatrix/views/GMAMatrix/GMAMatrix.ejs',
+function(){
+
+    // Namespacing conventions:
+    // AD.controllers.[application].[controller]
+    if (typeof AD.controllers.opstools == 'undefined') AD.controllers.opstools = {};
+    if (typeof AD.controllers.opstools.GMAMatrix == 'undefined') AD.controllers.opstools.GMAMatrix = {};
+    AD.controllers.opstools.GMAMatrix.Tool = AD.classes.opsportal.OpsTool.extend({
+
+
+        init: function (element, options) {
+            var self = this;
+            options = AD.defaults({
+                    templateDOM: 'opstools/GMAMatrix/views/GMAMatrix/GMAMatrix.ejs',
+                    resize_notification: 'gmamatrix.resize'
+            }, options);
+            this.options = options;
+
+            // Call parent init
+            AD.classes.opsportal.OpsTool.prototype.init.apply(this, arguments);
+
+
+            this.dataSource = this.options.dataSource; // AD.models.Projects;
+
+            this.initDOM();
+
+
+        },
+
+
+
+        initDOM: function () {
+
+            this.element.html(can.view(this.options.templateDOM, {} ));
+
+        },
+
+
+
+        '.ad-item-add click': function ($el, ev) {
+
+            ev.preventDefault();
+        },
+
+
+    });
+
+
+});
---
+++ assets/opstools/GMAMatrix/setup.js
@@ -0,0 +1,7 @@
+steal(
+        // List your Page's dependencies here:
+        'opstools/GMAMatrix/controllers/GMAMatrix.js',
+        '/site/labels/opstools/GMAMatrix'
+).then(function(){
+
+});
