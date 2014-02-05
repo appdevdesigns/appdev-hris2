2a31500329d1016431dcf84054e9ee35f40b9310
D. Shun-Luoi Fong <dsf@shunluoifong.com>
2014-02-05 14:01:18 +0700

    add files to .gitignore file, and changed CAS authentication from CAS to local

--- config/appdev.js
+++ config/appdev.js
@@ -11,8 +11,8 @@ module.exports.appdev = {
   // Which Type of authentication mechanism is in place for the current setup:
   // 'local' : basic username / password, successfully stored users session
   // 'CAS'   : utilize Central Authentication System
-  'authType': 'CAS',
-
+  //'authType': 'CAS',
+'authType': 'local',
 
   // Which language is the default language to use when none specified:
   'lang.default': 'en'
